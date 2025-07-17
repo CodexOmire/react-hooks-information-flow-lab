4a70ab8358e038e9d7d97117b011affc29ff1125
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-07-17 16:01:59 +0300

    Refactor: Created ShoppingList component with category filtering

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,29 +1,18 @@
 import React, { useState } from "react";
-import Item from "./Item";
+import Item from "./components/Item";
+import Filter from "./components/Filter";
+import itemsData from "./data/items";
 
-function ShoppingList({ items }) {
+function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
-
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+  const itemsToDisplay = itemsData.filter((item) => {
+    return selectedCategory === "All" || item.category === selectedCategory;
   });
 
   return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+    <div className="shopping-list">
+      <Filter onCategoryChange={setSelectedCategory} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />

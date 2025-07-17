3459648947a3439c4a3e0284e51ce95e274d509e
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-07-17 15:56:31 +0300

    Added item data for shopping list

--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,10 @@
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+  { id: 2, name: "Carrots", category: "Produce" },
+  { id: 3, name: "Cookies", category: "Dessert" },
+  { id: 4, name: "Milk", category: "Dairy" },
+  { id: 5, name: "Cake", category: "Dessert" },
+  { id: 6, name: "Broccoli", category: "Produce" }
 ];
 
 export default items;
